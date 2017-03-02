529ebb6a1c26bc487def7116866abeb3ae99883e
Rafka <fw.ge-mu.930@jb4.so-net.ne.jp>
2017-03-02 22:22:31 +0900

    Solved the problem 0059.

--- aoj/Program.cs
+++ aoj/Program.cs
@@ -3,7 +3,8 @@
 namespace aoj {
 	public class Program {
 		public static void Main() {
-			return;
+			var program = new _0059();
+			program.Run();
 		}
 	}
 }
---
+++ aoj/volume0/_0059.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+namespace aoj {
+	public class _0059 {
+		public _0059() {
+		}
+
+		public void Run() {
+			string str;
+			double[] vals;
+			while((str = Console.ReadLine()) != null) {
+				vals = str.Split().Take(8).Select(s => double.Parse(s)).ToArray();
+				Console.WriteLine((vals[0] <= vals[6] && vals[4] <= vals[2] && vals[1] <= vals[7] && vals[5] <= vals[3]) ? "YES" : "NO");
+			}
+		}
+	}
+}
