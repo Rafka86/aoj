f788e5ac30730d53259898b6cfd884387b2a4049
Rafka <fw.ge-mu.930@jb4.so-net.ne.jp>
2017-03-18 17:02:48 +0900

    コミット

---
+++ aoj/volume0/_0063.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aoj {
+	class _0063 {
+		public _0063() {
+		}
+
+		public void Run() {
+			string str;
+			int s = 0;
+			while((str = Console.ReadLine()) != null)
+				if (str == string.Join("", str.Reverse())) s++;
+			Console.WriteLine(s);
+		}
+	}
+}
