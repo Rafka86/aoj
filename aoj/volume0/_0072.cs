73e372a8e84d8e65fe178e9a7ffea4d9197a3d74
Rafka <fw.ge-mu.930@jb4.so-net.ne.jp>
2017-04-24 00:02:30 +0900

    Add files to solve. (This message has already used?)

--- aoj/volume0/_0072.cs
+++ aoj/volume0/_0072.cs
@@ -1,9 +1,17 @@
 ﻿using System;
 
 namespace aoj.volume0 {
+	struct Edge {
+		public int n1, n2, w;
+		public Edge(int node1, int node2, int weight) { n1 = node1; n2 = node2; w = weight; }
+	}
 	public class _0072 : Problem {
 		public override void Run() {
-			throw new NotImplementedException();
+			string str;
+			while((str = Console.ReadLine()) != "0") {
+				var n = int.Parse(str);
+				var m = int.Parse(Console.ReadLine());
+			}
 		}
 	}
 }
